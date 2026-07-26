00998b8d77762959490e125263374cc9bca2c07a
Nante <nantemino15@gmail.com>
2026-07-26 22:25:28 +0300

    update: modify experiences section layout and adjust card component styles

--- src/components/experiences/containers/ExperiencesRoot.tsx
+++ src/components/experiences/containers/ExperiencesRoot.tsx
@@ -1,9 +1,9 @@
 const ExperiencesRoot = () => {
   return (
-    <div className="experiences-root mt-10 h-[95vh]">
-      <h2 className="text-5xl font-black text-center text-green-primary">
+    <div className="experiences-root mt-10 ">
+      {/* <h2 className="text-5xl font-black text-center text-green-primary">
         Projets & Expériences
-      </h2>
+      </h2> */}
     </div>
   );
 };
